325cb125fcc8d945cbfc7d474a98fa4f6e8f7e3b
root <root@ip-172-31-30-215.ec2.internal>
2016-01-02 15:41:49 +0000

    added requirements setup.

---
+++ install-requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py 
+python2.7 ez_setup.py
+easy_install-2.7 pip
+
+yum -y install geos build-essential python27-devel libatlas-dev libatlas3gf-base
+pip2.7 install -r ~/eg_twitter/requirements.txt
+
+echo "PYSPARK_PYTHON=/usr/bin/python2.7" >> ~/spark/conf/spark-env.sh
+echo "PYSPARK_DRIVER_PYTHON=python2.7" >> ~/spark/conf/spark-env.sh
